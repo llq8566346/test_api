ae9a39a78d88c19aa139eccca35b26da80d36ac6
llq <994682157@qq.com>
2024-04-18 20:57:42 +0800

    llq

--- runtest.py
+++ runtest.py
@@ -1,3 +1,5 @@
+
+
 import unittest
 from library.HTMLTestRunnerNew import HTMLTestRunner
 from cases import test_register
@@ -6,6 +8,10 @@ from common.handle_email import handle_email
 from common.handlepath import REPORTSDIR
 import os
 # from BeautifulReport import BeautifulReport
+import datetime
+
+date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+
 
 suite = unittest.TestSuite()
 
@@ -14,14 +20,14 @@ loader = unittest.TestLoader()
 suite.addTest(loader.discover(CASESDIR))
 
 
-runner = HTMLTestRunner(stream=open("reports/reports2.html", "wb"),
+runner = HTMLTestRunner(stream=open("reports/{}reports2.html".format(date), "wb"),
                         title = "林亮钦的第二次测试报告",
                         description= "第二次测试报告",
                         tester = "林亮钦")
 
 runner.run(suite)
 
-filename = os.path.join(REPORTSDIR,"reports2.html")
+filename = os.path.join(REPORTSDIR,"{}reports2.html".format(date))
 
 handle_email(filename,"林亮钦的测试报告最终版")
 
@@ -32,7 +38,6 @@ handle_email(filename,"林亮钦的测试报告最终版")
 
 
            
-
 
 
 
